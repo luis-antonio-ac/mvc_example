8e58d316e93df04ec2dd94b45dbcd87812f8878c
Luis Antonio <luis.developer.ac@gmail.com>
2022-03-29 00:27:44 -0500

    adiciona a rota /users

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+
 import { IUser } from "../interfaces/user";
 import UserModel from "../model/user";
 
 export default class UserController {
   async create(request: Request, response: Response) {
+    const errors = validationResult(request);
+    if (!errors.isEmpty())
+      return response.status(400).json({ errors: errors.array() });
+
     const { name, email } = request.body as IUser;
 
     const userModel = new UserModel();
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
+
+import UserController from "./controllers/user";
 
 const routes = express.Router();
 
+const userController = new UserController();
+
 routes.get("/example", (req, res) =>
   res.json({ message: "example MVC Project" })
+);
+
+routes.post(
+  "/user",
+  body("email").isEmail(),
+  body("name").isLength({ min: 10 }),
+
+  userController.create
 );
 
 export { routes };
