9a1c95bd3c7920dca7948fe85c03e042df1b5d52
Luis Antonio <luis.developer.ac@gmail.com>
2022-03-28 23:07:29 -0500

    adiciona a configuracao de env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { routes } from "./routes";
 import cors from "cors";
+import dotenv from "dotenv";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+dotenv.config();
 
 app.listen(3000);
