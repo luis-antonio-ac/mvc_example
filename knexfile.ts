ec8df4e36dadb55cc324158889f1a3e5e378abed
Luis Antonio <luis.developer.ac@gmail.com>
2022-03-28 23:38:54 -0500

    corrige o arquivo knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,11 +1,17 @@
+import { Knex } from "knex";
 import path from "path";
 
 module.exports = {
-  client: "mysql",
-  connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
   },
-  migrations: {
-    directory: path.resolve(__dirname, "src", "database", "migrations"),
-  },
-};
+} as Knex.Config;
